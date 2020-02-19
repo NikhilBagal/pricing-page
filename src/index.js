c346d1fb9eb41f7cb72ce6bbe868cf28b2f08f52
nikhil bagal <nbagal02@gmail.com>
2020-02-19 16:28:22 +0530

    desktop layout built and functionality built

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 var value = document.querySelector('.switch input');
+var price = document.querySelectorAll('.price');
 value.addEventListener('change', function () {
-    // console.log(value.checked)
+    if (value.checked) {
+        price.forEach(function (item) { return check(item); });
+    }
+    else if (value.checked === false) {
+        price.forEach(function (item) { return check(item); });
+    }
 });
+function check(value) {
+    if (value.innerHTML === '$199.99') {
+        return value.innerHTML = '$19.99';
+    }
+    else if (value.innerHTML === '$249.99') {
+        return value.innerHTML = '$24.99';
+    }
+    else if (value.innerHTML === '$399.99') {
+        return value.innerHTML = '$39.99';
+    }
+    else if (value.innerHTML === '$19.99') {
+        return value.innerHTML = '$199.99';
+    }
+    else if (value.innerHTML === '$24.99') {
+        return value.innerHTML = '$249.99';
+    }
+    else if (value.innerHTML === '$39.99') {
+        return value.innerHTML = '$399.99';
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-const value = document.querySelector('.switch input')
+const value = <HTMLInputElement>document.querySelector('.switch input');
+const price = document.querySelectorAll('.price');
 value.addEventListener('change',() => {
-   // console.log(value.checked)
+    if(value.checked) {
+      price.forEach(item => check(item))
+    }else if (value.checked === false){
+       price.forEach(item => check(item))
+    }
 })
+function check(value:any){
+   if(value.innerHTML === '$199.99'){
+      return value.innerHTML='$19.99'
+   }else if(value.innerHTML === '$249.99'){
+      return value.innerHTML='$24.99'
+   }else if(value.innerHTML === '$399.99'){
+      return value.innerHTML='$39.99'
+   }else if(value.innerHTML === '$19.99'){
+      return value.innerHTML = '$199.99'
+   }else if(value.innerHTML === '$24.99'){
+      return value.innerHTML = '$249.99'
+   }else if(value.innerHTML === '$39.99'){
+      return value.innerHTML = '$399.99'
+   }
+}
+
